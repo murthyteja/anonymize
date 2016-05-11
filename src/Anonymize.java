88819f2b99aedefb2357dcfed3f0a034a8dc7abf
Murthy Upadhyayula <murthy.polymath@gmail.com>
2016-05-11 23:27:37 +0200

    Basic Connection Program Added

--- src/Anonymize.java
+++ src/Anonymize.java
@@ -8,7 +8,7 @@ public class Anonymize {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first Java program");
 		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-		Connection conn = DriverManager.getConnection("jdbc:ucanaccess://<mdb or accdb file path>","", "");
+		Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/murthyteja/Desktop/Database1.accdb");
 	}
 
 }
