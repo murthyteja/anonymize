a21d9022980af5ed54b8bf117d27038f2c67fd3a
Murthy Upadhyayula <murthyupadhyayula@Murthys-MBP.fritz.box>
2016-05-11 22:16:17 +0200

    Initial Commit
    * Added necessary JAR files to establish a basic connection
    * Added Anonymize.java

---
+++ src/Anonymize.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Anonymize {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first Java program");
+		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		Connection conn = DriverManager.getConnection("jdbc:ucanaccess://<mdb or accdb file path>","", "");
+	}
+
+}
