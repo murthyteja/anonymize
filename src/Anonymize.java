5f4364b0c4d0f21999d543d0f97d547cf453b25f
Murthy Upadhyayula <murthy.polymath@gmail.com>
2016-05-12 22:18:50 +0200

    Testing DatabaseMetadata with Access

--- src/Anonymize.java
+++ src/Anonymize.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Anonymize {
@@ -9,6 +11,12 @@ public class Anonymize {
 		System.out.println("This is my first Java program");
 		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 		Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/murthyteja/Desktop/Database1.accdb");
+		DatabaseMetaData md = conn.getMetaData();
+		ResultSet rs = md.getTables(null, null, "%", null);
+		System.out.println("Tables to be anonymized are:");
+		while (rs.next()){
+			System.out.println(rs.getString(3));
+		}
 	}
 
 }
