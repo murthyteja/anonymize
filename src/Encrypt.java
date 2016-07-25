b435d9df908550364eab7bd545306b9ede082ba9
Murthy Upadhyayula <murthy.polymath@gmail.com>
2016-07-25 18:10:09 +0200

    Update Encrypt.java

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -5,20 +5,75 @@ import java.util.Base64;
 public class Encrypt {
 
 	public static String encodeString(String input) throws UnsupportedEncodingException{
+		String encryptedText;
+		if (Utilities.isEmail(input)){
+			encryptedText = encodeEmail(input);
+		}
+		else{
+			encryptedText = encodeText(input);
+		}
+		return encryptedText;
+	}
+
+	public static String decodeString(String input) throws UnsupportedEncodingException{
+		String decryptedText;
+		if (Utilities.isEmail(input)){
+			decryptedText = decodeEmail(input);
+		}
+		else{
+			decryptedText = decodeText(input);
+		}
+		return decryptedText;
+	}
+
+	public static double encodeDouble(double number){
+		AppProperties appProperties = new AppProperties();
+		int divisor = Integer.parseInt(appProperties.getProperty("number_divisor"));
+		return number / divisor;
+	}
+
+	public static double decodeDouble(double number){
+		AppProperties appProperties = new AppProperties();
+		int divisor = Integer.parseInt(appProperties.getProperty("number_divisor"));
+		return number * divisor;
+	}
+
+	public static double encodeInteger(double number){
+		AppProperties appProperties = new AppProperties();
+		int divisor = Integer.parseInt(appProperties.getProperty("number_divisor"));
+		return number / divisor;
+	}
+
+	public static double decodeInteger(double number){
+		AppProperties appProperties = new AppProperties();
+		int divisor = Integer.parseInt(appProperties.getProperty("number_divisor"));
+		return number * divisor;
+	}
+	
+	private static String encodeEmail(String email) throws UnsupportedEncodingException{
+		String[] chunks = email.split("@");
+		// Consider mark@facebook.com, mailId = "mark" and restOfEmail = "facebook.com"
+		String mailId = chunks[0];
+		String restOfEmail = chunks[1];
+		// To encrypt an email, encrypt the ID alone
+		return (encodeText(mailId) + "@" + restOfEmail);
+	}
+	
+	private static String decodeEmail(String encodedEmail) throws UnsupportedEncodingException{
+		String[] chunks = encodedEmail.split("@");
+		String encodedMailId = chunks[0];
+		String restOfEmail = chunks[1];
+		// To decrypt an email, decrypt the ID alone
+		return (decodeText(encodedMailId) + "@" + restOfEmail);
+	}
+	
+	private static String encodeText(String input) throws UnsupportedEncodingException{
 		byte[] bytesEncoded = Base64.getEncoder().encode(input.getBytes());
 		return new String(bytesEncoded, "UTF-8");
 	}
 	
-	public static String decodeString(String input) throws UnsupportedEncodingException{
+	private static String decodeText(String input) throws UnsupportedEncodingException{
 		byte[] decodedBytes = Base64.getDecoder().decode(input.getBytes());
 		return new String(decodedBytes, "UTF-8");
-	}
-	
-	public static double encodeDouble(double number){
-		return number/12;
-	}
-
-	public static double decodeDouble(double number){
-		return number * 12;
 	}
 }
