1f2de4efa06114213279882f4738cebad42398c7
Murthy Upadhyayula <murthy.polymath@gmail.com>
2016-07-26 16:08:57 +0200

    Update DataSource.java

--- src/DataSource.java
+++ src/DataSource.java
@@ -6,18 +6,27 @@ public class DataSource {
 	String driverClassPath;
 	String connectionString;
 	AppProperties appProperties;
+	ErrorLogger errorLogger;
+	
+	/* Public: Constructor for Data Source. This function picks up the driver and
+	 * 		   connection string details from the config.properties file and tries
+	 * 		   to establish a connection to the database.
+	 */
 	public DataSource(){
+		System.out.println("Connecting to the Data Source......");
 		appProperties = new AppProperties();
-		System.out.println("Constructor for Data Source");
-		driverClassPath = "net.ucanaccess.jdbc.UcanaccessDriver";
-		connectionString = "jdbc:ucanaccess://H:/Work/Anonymiye_Test/B23.mdb";
+		errorLogger = new ErrorLogger();
+		driverClassPath = appProperties.getProperty("driver_class");
 		connectionString = appProperties.getProperty("connection_string");
 		try{
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			conn = DriverManager.getConnection("jdbc:ucanaccess://C:/Users/murthyteja/Desktop/sample.accdb");
+			Class.forName(driverClassPath);
+			conn = DriverManager.getConnection(connectionString);
+			System.out.println("Connecting Established......");
 		}
 		catch(Exception exp){
 			System.out.println("Something went wrong: " + exp.getMessage());
+			errorLogger.write("Failed to connect to the data source:");
+			errorLogger.write(exp.getMessage());
 		}
 	}
 }
