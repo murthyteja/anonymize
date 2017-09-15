a1c1358e742a08e5fca016a2abb0ecedf2142f02
Murthy Upadhyayula <murthy.polymath@gmail.com>
2017-09-15 18:14:43 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,44 @@
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
+	
+	/*
+	 * Private: Static method that prints out summary information on the console
+	 * 
+	 * Returns nothing
+	 */
+	private static void printSummary(long start, long end){
+		System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("Anonymization Process has been completed");
+		System.out.println("Total Time Taken: " + (end - start)/1000f + " Seconds");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	}
 
-	public static void main(String[] args) throws UnsupportedEncodingException {
-		// Initialize Application Properties Object
-		AppProperties app = new AppProperties();
+	/*
+	 * Public: Main function for the Anonymization application
+	 * 
+	 * Returns nothing
+	 */
+	public static void main(String[] args){
+
+		// Initialize the loggers
+		AppLogger appLogger = new AppLogger("app_log_path");
+		AppLogger errorLogger = new AppLogger("error_log_path");
+		AppLogger statusLogger = new AppLogger("status_log_path");
 		
-		// Testing a list of Tables to be ignored
-		List<String> ignoreTablesList = new ArrayList<String>();
-		ignoreTablesList.add("GlobalSettings");
 		// Initialize the Data Source Object
-		DataSource ds = new DataSource();
-		Anonymize.anonymizeDatabase(ds.conn, ignoreTablesList);
+		DataSource ds = new DataSource(appLogger, errorLogger, statusLogger);
+
+		System.out.println("Starting Anonymization Process");
+		long start = System.currentTimeMillis();
+		// Call the Big Daddy. Let him do all the heavy lifting
+		Anonymize anonymizeObject = new Anonymize(appLogger, errorLogger, statusLogger);
+		//Anonymize.anonymizeDatabase(ds.conn);
+		anonymizeObject.anonymizeDatabase(ds.conn);
+		long end = System.currentTimeMillis();
+		printSummary(start, end);
+
+		appLogger.closeFileHandler();
+		errorLogger.closeFileHandler();
+		statusLogger.closeFileHandler();
+		
 	}
 }
